407476dcd85e6ae394b08a7e753d94dc14bb3698
Zandre Witte <zandre.witte@gmail.com>
2019-01-30 11:14:16 +0100

    added choco to gitlab-ci yaml file, updated vagrant bootstrap script to install msys2 and mingw, updated paths in windows_build.cmd to point to installed choco tools

--- Vagrantfile
+++ Vagrantfile
@@ -42,7 +42,6 @@ Vagrant.configure("2") do |config|
 
     agent1.vm.network :private_network, ip: "192.168.56.103"
 
-    config.vm.synced_folder "../../../..", "/opt/stackstate-go"
     config.vm.provision "shell", path: "bootstrap.ps1"
 
     agent1.vm.provider :virtualbox do |v|
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,17 +1,23 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -y git hg golang dep ruby mingw netcat
+choco install -y git hg golang dep ruby msys2 mingw netcat
 
-setx GOPATH "C:\opt\stackstate-go"
-setx GO_PROCESS_AGENT "C:\opt\stackstate-go\src\github.com\StackVista\stackstate-process-agent"
-setx path "%path%;%GOPATH%\bin"
+[string]$source  = 'C:\vagrant'
+[string]$destination = 'C:\opt\stackstate-go\src\github.com\StackVista\stackstate-process-agent'
+mkdir -Force $destination
+Copy-Item -Path (Get-Item -Path "$source\*" -Exclude ('vendor', '.idea', '.circleci') -Force).FullName -Destination $destination -Recurse -Force
+
+[string]$gopath = 'C:\opt\stackstate-go'
+setx GOPATH $gopath
+setx GO_PROCESS_AGENT $destination
+setx path "%path%;$gopath\bin"
 setx DEPNOLOCK 1
 
-refreshenv
+$profile
 
 if (Get-Command rake -errorAction SilentlyContinue)
 {
     rake --version
 } else {
-    C:\tools\ruby26\bin\gem install rake
+    C:\tools\ruby26\bin\gem.cmd install rake
 }

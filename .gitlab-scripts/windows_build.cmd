44d7d7f57e151ec1016d1ea93b144eb7931773b1
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2019-03-21 17:12:35 +0100

    chore(build) Remove unused variables and make sure to run test on windows

--- .gitlab-scripts/windows_build.cmd
+++ .gitlab-scripts/windows_build.cmd
@@ -7,4 +7,4 @@ set "MINGW_PACKAGE_PREFIX=mingw-w64-x86_64"
 set "LANG=en_US.UTF-8"
 set "PATH=c:\tools\msys64\mingw64\bin;c:\tools\msys64\usr\bin;%PATH%"
 echo %CD%
-call rake build windres=true --trace
+call rake ci windres=true --trace

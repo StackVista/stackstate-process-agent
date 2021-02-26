5b57ac129f8b441abf99a09bdb0739558b8dbcbd
zandrewitte <zandre.witte@gmail.com>
2021-02-26 10:45:57 +0100

    retry tracer tests + configuration

--- checks/net_linux.go
+++ checks/net_linux.go
@@ -12,7 +12,6 @@ import (
 	log "github.com/cihub/seelog"
 	"github.com/patrickmn/go-cache"
 	"os"
-	"time"
 )
 
 // Init initializes a ConnectionsCheck instance.
@@ -39,7 +38,7 @@ func (c *ConnectionsCheck) Init(cfg *config.AgentConfig, sysInfo *model.SystemIn
 		conf.MaxConnections = cfg.MaxPerMessage
 		conf.BackfillFromProc = cfg.NetworkInitialConnectionsFromProc
 
-		t, err := retryTracerInit(conf)
+		t, err := retryTracerInit(cfg.NetworkTracerInitRetryDuration, cfg.NetworkTracerInitRetryAmount, conf, tracer.NewTracer)
 		if err != nil {
 			log.Errorf("failed to create network tracer: %s.  Set the environment STS_NETWORK_TRACING_ENABLED to false to disable network connections reporting", err)
 			return
@@ -56,26 +55,4 @@ func (c *ConnectionsCheck) Init(cfg *config.AgentConfig, sysInfo *model.SystemIn
 	c.cache = cache.New(cfg.NetworkRelationCacheDurationMin, cfg.NetworkRelationCacheDurationMin)
 
 	c.buf = new(bytes.Buffer)
-}
-
-func retryTracerInit(conf *tracerConfig.Config) (tracer.Tracer, error) {
-	retryTicker := time.NewTicker(5 * time.Second)
-	retriesLeft := 3
-
-	var t tracer.Tracer
-	var err error
-
-retry:
-	for {
-		select {
-		case <-retryTicker.C:
-			t, err = tracer.NewTracer(conf)
-			if err == nil || retriesLeft == 0 {
-				break retry
-			}
-			retriesLeft = retriesLeft - 1
-		}
-	}
-
-	return t, err
 }

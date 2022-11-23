72bb2b32bd8db9b9a3bed0890103533f96e1213c
Juliano Krieger <julianosk@gmail.com>
2022-11-23 14:15:36 +0000

    Improve devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-go get github.com/awalterschulze/goderive
-go generate ./...
+# go get github.com/awalterschulze/goderive
+# go generate ./...
 
 sudo apt-get update
-sudo apt-get install rake
+sudo apt-get -y install rake

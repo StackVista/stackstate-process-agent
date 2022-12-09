fba41739f8c26c8a4b9007fc79e09663e77006cc
Alexander Hasselbach <agasselbakh@stackstate.com>
2022-12-09 16:32:24 +0300

    better docker output

--- packaging/publish_image.sh
+++ packaging/publish_image.sh
@@ -13,20 +13,28 @@ ORGANIZATION="stackstate"
 echo "IMAGE_TAG=${IMAGE_TAG}"
 echo "IMAGE_REPO=${IMAGE_REPO}"
 
-docker login -u "${docker_user}" -p "${docker_password}" "${REGISTRY_DOCKERHUB}"
-docker login -u "${quay_user}" -p "${quay_password}" "${REGISTRY_QUAY}"
+PUBLISHED=""
+
+docker_tag_and_push () {
+  docker tag "${BUILD_TAG}" "$1"
+  docker push "$1"
+  PUBLISHED="$PUBLISHED $1"
+}
 
 for REGISTRY in "${REGISTRY_DOCKERHUB}" "${REGISTRY_QUAY}"; do
     DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
-
-    docker tag "${BUILD_TAG}" "${DOCKER_TAG}"
-    docker push "${DOCKER_TAG}"
+    docker_tag_and_push "${DOCKER_TAG}"
 
     if [ -n "$EXTRA_TAG" ]; then
         DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
-        docker tag "${DOCKER_TAG}" "${DOCKER_EXTRA_TAG}"
-        echo "Pushing release to ${EXTRA_TAG}"
-        docker push "${DOCKER_EXTRA_TAG}"
+        docker_tag_and_push "${DOCKER_EXTRA_TAG}"
     fi
 done
 
+set +x
+echo ""
+echo "Published images:"
+for tag in ${PUBLISHED}
+do
+  echo "        $tag"
+done

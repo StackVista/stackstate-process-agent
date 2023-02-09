453b10b5973cb7f82c5c8abf3312cc16d069a0bf
Louis Parkin <lparkin@stackstate.com>
2023-02-09 14:47:06 +0100

    STAC-19061 Updates from MR comments.

--- packaging/publish_image.sh
+++ packaging/publish_image.sh
@@ -6,7 +6,6 @@ BUILD_TAG="${1}"
 IMAGE_REPO="${2}"
 IMAGE_TAG="${3}"
 EXTRA_TAG="${4}"
-K8S_REPO="${5}"
 REGISTRY="quay.io"
 ORGANIZATION="stackstate"
 
@@ -28,12 +27,6 @@ docker_tag_and_push "${DOCKER_TAG}"
 if [ -n "$EXTRA_TAG" ]; then
     DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
     docker_tag_and_push "${DOCKER_EXTRA_TAG}"
-
-    # If K8S_REPO is not equal to "NOP" and is set then push the image to the k8s repo
-    if [ -n "${K8S_REPO}" ] && [ "${K8S_REPO}" != "NOP" ]; then
-        DOCKER_K8S_TAG="${REGISTRY}/${ORGANIZATION}/${K8S_REPO}:${CI_COMMIT_SHORT_SHA}"
-        docker_tag_and_push "${DOCKER_K8S_TAG}"
-    fi
 fi
 
 set +x

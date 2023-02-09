00db33c8a04d2abe90890b69722afefe13870727
Louis Parkin <lparkin@stackstate.com>
2023-02-09 11:00:49 +0100

    STAC-19061 Add k8s-tagged image, remove dockerhub build/tag/push.

--- packaging/publish_image.sh
+++ packaging/publish_image.sh
@@ -6,8 +6,8 @@ BUILD_TAG="${1}"
 IMAGE_REPO="${2}"
 IMAGE_TAG="${3}"
 EXTRA_TAG="${4}"
-REGISTRY_DOCKERHUB="docker.io"
-REGISTRY_QUAY="quay.io"
+K8S_REPO="${5}"
+REGISTRY="quay.io"
 ORGANIZATION="stackstate"
 
 echo "IMAGE_TAG=${IMAGE_TAG}"
@@ -21,15 +21,20 @@ docker_tag_and_push () {
   PUBLISHED="$PUBLISHED $1"
 }
 
-for REGISTRY in "${REGISTRY_DOCKERHUB}" "${REGISTRY_QUAY}"; do
-    DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
-    docker_tag_and_push "${DOCKER_TAG}"
 
-    if [ -n "$EXTRA_TAG" ]; then
-        DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
-        docker_tag_and_push "${DOCKER_EXTRA_TAG}"
+DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
+docker_tag_and_push "${DOCKER_TAG}"
+
+if [ -n "$EXTRA_TAG" ]; then
+    DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    docker_tag_and_push "${DOCKER_EXTRA_TAG}"
+
+    # If K8S_REPO is not equal to "NOP" and is set then push the image to the k8s repo
+    if [ -n "${K8S_REPO}" ] && [ "${K8S_REPO}" != "NOP" ]; then
+        DOCKER_K8S_TAG="${REGISTRY}/${ORGANIZATION}/${K8S_REPO}:${EXTRA_TAG}"
+        docker_tag_and_push "${DOCKER_K8S_TAG}"
     fi
-done
+fi
 
 set +x
 echo ""

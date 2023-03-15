414f319efcf69f78c8804f517c5a107c04cef7f4
Bram Schuur <bschuur@stackstate.com>
2023-03-15 11:19:23 +0100

    STAC-19271: Add script for generating go/ebpf code from agent

---
+++ prebuild-datadog-agent-scripts/run-datadog-agent-prebuild.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# This script is used by prebuild-datadog-agent and CI to generate the prebuild files from a docker container
+
+set -ex
+
+WORKDIR="/workdir-datadog-agent"
+
+# This command assumes the datadog agent to be mounted at /source-datadog-agent. To avoid outputting to that directory,
+# we make a clone before running any commands
+mkdir $WORKDIR
+cp -a "$SOURCEDIR"/. $WORKDIR
+chown -R root:root $WORKDIR
+cd $WORKDIR
+
+# Adding a faux tag to make the build pass on the rpo with no tags
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git tag -a 7.0.0 -m 7.0.0
+
+# This command will create system-probe. Running the go:generate as well as invoking the precompilation of the ebpf files
+invoke system-probe.build
+
+# Output the generated gofiles (including relative paths) to the output directory
+mkdir -p "$OUTPUTDIR/gofiles"
+git ls-files --others --ignored --exclude-from=.gitignore | grep "\.go$" | xargs -I{} cp --parents {} "$OUTPUTDIR/gofiles"
+chown -R "$OUTPUT_USER_ID:$OUTPUT_GROUP_ID" "$OUTPUTDIR/gofiles"
+
+# Output the ebpf files to the output directory
+mkdir -p "$OUTPUTDIR/ebpf/"
+cp -r ./pkg/ebpf/bytecode/build/* "$OUTPUTDIR/ebpf/"
+chown -R "$OUTPUT_USER_ID:$OUTPUT_GROUP_ID" "$OUTPUTDIR/ebpf"
+
---
+++ prebuild-datadog-agent.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+
+# Okay, here it goes: The process agent depends on the datadog agent, which:
+# - for some modules (EBPF) uses go:generate which needs to be run to even build the dependency.
+# - for some other modules generate EBPF code, which we need but is not part of the regular go build
+# This file makes sure those prebuild steps (go:generate and ebpf files) get built and installed.
+
+#!/bin/bash
+
+set -e
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+if [[ "$DIR" = "." ]]; then DIR="$PWD"; fi
+
+function printUsage() {
+  cat << USAGE
+
+Usage: ./prebuild-datadog-agent.sh --generate|--clean|--shell|--install-go|--help
+
+This script run the prebuild steps for the datadog agent dependency. Those steps are not automatically invoked by the go build.
+  -g/--generate   -- Generate the prebuild files
+  -c/--clean      -- Clean the generated files
+  -i/--install-go -- Install the prebuild go files
+  -s/--shell      -- Launch into a shell in which the prebuild files are built
+  -h/--help       -- Print this help page
+
+USAGE
+}
+
+ACTION=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -g|--generate)
+      ACTION="generate"
+      shift
+    ;;
+    -i|--install-go)
+      ACTION="install-go"
+      shift
+    ;;
+    -s|--shell)
+      ACTION="shell"
+      shift
+    ;;
+    -c|--clean)
+      ACTION="clean"
+      shift
+    ;;
+    -h|--help)
+      ACTION="help"
+      shift
+    ;;
+    *)
+      ACTION="$1"
+      shift
+      break
+    ;;
+  esac
+done
+
+ALL_ARTIFACTS_DIR="$DIR/prebuild_artifacts"
+GO_MOD_DEPENDENCY_DIR=$(go list -f '{{ .Replace.Dir }}' -m github.com/DataDog/datadog-agent)
+
+if [ "$GO_MOD_DEPENDENCY_DIR" = "" ]; then
+  echo "No dependency directory found for github.com/DataDog/datadog-agent. Maybe run go get first?"
+  exit 1
+elif [ -d "$GO_MOD_DEPENDENCY_DIR/.git" ]; then
+  echo "Running the data prebuild for a local dependency $DEPENDENCY_VERSION. Be aware that generate will not automatically pickup changes. Be sure to run -clean whenever the generated code would change."
+  # The dependency is a local git repo. No need to pull or pick a version
+  DEPENDENCY_VERSION="local"
+  SOURCE_DIR=$GO_MOD_DEPENDENCY_DIR
+else
+  DEPENDENCY_VERSION=$(go list -f '{{ .Replace.Version }}' -m github.com/DataDog/datadog-agent)
+  SOURCE_DIR="$ALL_ARTIFACTS_DIR/$DEPENDENCY_VERSION/checkout"
+fi
+
+DEPENDENCY_ARTIFACTS_DIR="$ALL_ARTIFACTS_DIR/$DEPENDENCY_VERSION"
+
+function runPrebuild() {
+  if [ ! -d "$SOURCE_DIR" ]; then
+    echo "datadog-agent-upstream-for-process-agent was not cloned, cloning"
+    mkdir -p $DEPENDENCY_ARTIFACTS_DIR
+    GIT_VERSION=$(echo "$DEPENDENCY_VERSION" | cut -d'-' -f 3)
+    (cd $DEPENDENCY_ARTIFACTS_DIR &&
+      git clone https://github.com/StackVista/datadog-agent-upstream-for-process-agent checkout &&
+      cd checkout &&
+      git checkout "$GIT_VERSION")
+  fi
+
+  set -x
+  docker run \
+    -e "OUTPUT_USER_ID=$(id -u "${USER}")" \
+    -e "OUTPUT_GROUP_ID=$(id -g "${USER}")" \
+    -v "$SOURCE_DIR":/source-datadog-agent \
+    -e SOURCEDIR="/source-datadog-agent" \
+    -v "$DEPENDENCY_ARTIFACTS_DIR":/output \
+    -e OUTPUTDIR="/output" \
+    -v "$DIR/prebuild-datadog-agent-scripts":/scripts \
+    "$@"
+  set +x
+}
+
+if [[ "$ACTION" = "generate" ]]; then
+  echo "Generating code"
+  if [ -d "$DEPENDENCY_ARTIFACTS_DIR" ]; then
+    echo "Prebuild artifacts were already generated. Skipping. To regenerate first run with --clean"
+  fi
+
+  mkdir -p "$DEPENDENCY_ARTIFACTS_DIR"
+  runPrebuild mytest:latest /scripts/run-datadog-agent-prebuild.sh
+elif [[ "$ACTION" = "install-go" ]]; then
+  echo "Installing go files"
+  if [ ! -d "$DEPENDENCY_ARTIFACTS_DIR/gofiles" ]; then
+    echo "No generated files found at $DEPENDENCY_ARTIFACTS_DIR/golang, please run --generate first"
+    exit 1
+  fi
+
+  chmod -R ug+w "$GO_MOD_DEPENDENCY_DIR"
+  cp -a "$DEPENDENCY_ARTIFACTS_DIR/gofiles"/* "$GO_MOD_DEPENDENCY_DIR"
+elif [[ "$ACTION" = "clean" ]]; then
+  echo "Cleaning prebuild files from $PREBUILD_ARTIFACTS_DIR"
+  rm -rf "$ALL_ARTIFACTS_DIR"
+elif [[ "$ACTION" = "shell" ]]; then
+  echo "Launching generate shell"
+  runPrebuild -it mytest:latest /bin/bash
+elif [[ -z "$ACTION" ]]; then
+  echo "No argument was passed"
+  printUsage
+  exit 1
+else
+  echo "Illegal argument $ACTION"
+  printUsage
+  exit 1
+fi

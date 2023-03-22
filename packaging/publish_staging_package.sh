d3731ddfd619c05c9795273f366af9a154c53db8
Bram Schuur <bschuur@stackstate.com>
2023-03-22 15:50:42 +0100

    STAC-19271: Add publishing of ebpf artifacts

--- packaging/publish_staging_package.sh
+++ packaging/publish_staging_package.sh
@@ -12,7 +12,14 @@ if [ -z ${PROCESS_AGENT_VERSION+x} ]; then
 fi
 echo $PROCESS_AGENT_VERSION
 FILENAME="process-agent-amd64-$PROCESS_AGENT_VERSION"
+EBPF_FILENAME="process-agent-amd64-$PROCESS_AGENT_VERSION-ebpf.tar.gz"
 WORKSPACE=${WORKSPACE:-$PWD/../}
 agent_path="$WORKSPACE"
 
+echo "Uploading agent binary"
+
 aws s3 cp $agent_path/process-agent s3://${STS_AWS_BUCKET:-stackstate-process-agent-test}/binaries/${PACKAGING_BRANCH:-dirty}/$FILENAME --acl public-read
+
+tar -cfz $agent_path/ebpf-object-files.tar.gz $agent_path/ebpf-object-files
+
+aws s3 cp $agent_path/ebpf-object-files.tar.gz s3://${STS_AWS_BUCKET:-stackstate-process-agent-test}/binaries/${PACKAGING_BRANCH:-dirty}/$EBPF_FILENAME --acl public-read

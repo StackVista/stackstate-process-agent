b9562aef3a737c2f9cd2a226a138ca9ed2c9f1be
Bram Schuur <bschuur@stackstate.com>
2023-03-23 10:19:09 +0100

    STAC-19271: Use upstream build container

---
+++ build-container.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This script launches into a docker container with the current directory mounted to debug builds
+
+docker run -v "$PWD:/source" -it quay.io/stackstate/datadog_build_deb_x64:3e691150 bash
